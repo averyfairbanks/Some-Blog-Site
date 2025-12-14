eb965daa25018e25b157860e5c5a24d150dea577
Avery Fairbanks <avery@averyf.dev>
2025-12-14 16:00:52 -0500

    maybe init?

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
+import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRouter(), tsconfigPaths()],
+  ssr: {
+    // Workaround for resolving dependencies in the server bundle
+    // Without this, the React context will be different between direct import and transitive imports in development environment
+    // For more information, see https://github.com/mui/material-ui/issues/45878#issuecomment-2987441663
+    optimizeDeps: {
+      include: ['@emotion/*', '@mui/*'],
+    },
+    noExternal: ['@emotion/*', '@mui/*'],
+  },
 });
